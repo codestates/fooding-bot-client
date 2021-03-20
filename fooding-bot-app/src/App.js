289cac195185012dadf0509ba2cabc296033405b
ShinHeegook <hgsignal@gmail.com>
2021-03-20 17:37:30 +0900

    add Signup page layout

---
+++ fooding-bot-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SignupContainer from './pages/SignupContainer';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <SignupContainer />
+            </div>
+        )
+    }
+}
+
+export default App;
