152afe02997f97394f1ca93d93fdfa4ac808b71a
coderealsw <coderealsw@gmail.com>
2021-03-23 11:05:51 +0900

    edit comflict

--- fooding-bot-app/src/App.js
+++ fooding-bot-app/src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import './App.css';
-<<<<<<< HEAD
-import LoginContainer from './pages/LoginContainer';
+// import LoginContainer from './pages/LoginContainer';
 // import SignupContainer from './pages/SignupContainer';
-=======
-import SignupContainer from './pages/SignupContainer';
->>>>>>> 7bf7b8580b76aa21bceca9f91748002bdd5e3981
 // import ProfileContainer from './pages/ProfileContainer'
 // import MyFoodingContainer from './pages/MyFoodingContainer';
 // import PasswordContainer from './pages/PasswordContainer';
-<<<<<<< HEAD
-=======
 // import MypageContainer from './pages/MypageContainer.js';
->>>>>>> c1de29ab2544649f2ba99dbbd4a5eba336890b29
 
 class App extends React.Component {
 
@@ -20,19 +13,12 @@ class App extends React.Component {
         return (
             <div>
                 <div className='app-background'>
-<<<<<<< HEAD
-                    <LoginContainer />
+                    {/* <LoginContainer /> */}
                     {/* <SignupContainer /> */}
-=======
-                    <SignupContainer />
->>>>>>> 7bf7b8580b76aa21bceca9f91748002bdd5e3981
                     {/* <ProfileContainer /> */}
                     {/* <MyFoodingContainer /> */}
                     {/* <PasswordContainer /> */}
-<<<<<<< HEAD
-=======
                     {/* <MypageContainer /> */}
->>>>>>> c1de29ab2544649f2ba99dbbd4a5eba336890b29
                 </div>
             </div>
         )
--- fooding-bot-app/src/pages/LoginContainer.js
+++ fooding-bot-app/src/pages/LoginContainer.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import fooding from '../images/fooding-icon.png';
 
 function Login() {
@@ -35,31 +34,6 @@ function Login() {
       </div>
 
     <div className='team-name'>by R.F.D</div>
-=======
-
-function Login() {
-  return (
-    <div className='app-background'>
-      <div className='login-title'>Fooding-Bot</div>
-      <div classNmae='logo'>logo</div>
-
-      <div className='login-input-wrap'>Login to your Account
-          <input className='input' type='email' placehodler='이메일'></input>
-          <input className='input' type='password' placehodler='비밀번호'></input>
-        </div>
-
-        
-      <div>자동 로그인 </div>
-      <button className='btn login'>로그인</button>
-
-      <div className='btn-wrap'>
-      <button className='btn'>소셜 로그인</button>
-      <button className='btn'>비회원 로그인</button>
-      <button className='btn' type='submt'>회원가입</button>
-      </div>
-
-    <div className='maker'>by R.F.D</div>
->>>>>>> c1de29ab2544649f2ba99dbbd4a5eba336890b29
 
     </div>
   )
