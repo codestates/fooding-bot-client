42b9e4c0195a3b95f1b048b17793a8f8c7d5b588
ShinHeegook <hgsignal@gmail.com>
2021-03-24 10:50:59 +0900

    add Signup page password check warnig ment modify

--- fooding-bot-app/src/pages/SignupContainer.js
+++ fooding-bot-app/src/pages/SignupContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import undo from '../images/undo.png';
 import searchIcon from '../images/search-icon.png';
+import warning from '../images/warning-sign-icon.png';
 
 function SignupContainer() {
     return (
@@ -12,15 +13,24 @@ function SignupContainer() {
 
             <div className='signup-input-wrap'>
                 <div className='signup-input-box'>
-                    <div><input type='text' placeholder="이름" /></div>
-                    <div><input type='email' placeholder="이메일" /></div>
+                    <div><input type='text' placeholder="이름" maxLength="6" /></div>
+                    <div>
+                        <input type='email' placeholder="이메일" maxLength="20" />
+                    </div>
+
                     <button className='signup-email-check-button' >중복확인</button>
 
-                    <div><input type='password' placeholder="비밀번호" /></div>
-                    <label>비밀번호는 8자 이상으로 입력해주세요.</label>
 
-                    <div><input type='password' placeholder="비밀번호 확인" /></div>
-                    <label>입력하신 비밀번호가 다릅니다. 다시 한번 확인해주세요.</label>
+                    <div><input id='pw' type='password' placeholder="비밀번호" maxLength="12" /></div>
+                    <label htmlFor='pw'>비밀번호는 8자 이상으로 입력해주세요.</label>
+
+                    <div><input id='pw-check' type='password' placeholder="비밀번호 확인" maxLength="12" /></div>
+                    <label htmlFor='pw-check' className='pw-check'>
+                        <img className='warning-icon' src={warning} />
+                        <span className='warning-ment'>
+                            입력하신 비밀번호가 다릅니다. 다시 확인해주세요.
+                        </span>
+                    </label>
 
                     <div className='signup-input-address'><input type='text' placeholder='주소' />
                         <span className='icon'>
